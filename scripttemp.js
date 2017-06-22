a4e8190a52433f7ad3966b79a8e971a59111368a
Unknown <fabien.mazette@gmail.com>
2017-06-22 17:18:52 +0200

    Add scripttemp.js

---
+++ scripttemp.js
@@ -0,0 +1,7 @@
+$("#cash").text(cash);
+$("#sales").text(sales);
+$("#profit").text(profit);
+$("#drinkOffered").text(drinkOffered);
+$("#name").text(name);
+$("#latitude").text(latitude);
+$("#longitude").text(longitude);
