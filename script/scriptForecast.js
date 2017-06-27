7564d5f84477af4bb57899e415d04725f0a9cbec
Louis <louis.schneider@imerir.com>
2017-06-27 16:03:03 +0200

    Finishing forecast script, working successfully

--- script/scriptForecast.js
+++ script/scriptForecast.js
@@ -2,57 +2,109 @@ var gameInfo = JSON.parse(localStorage.getItem("gameInfo"));
 console.log(gameInfo);
 var domain = "http://webserverlemonade.herokuapp.com";
 
-function displayForecast{
-  var myImg = new Image
+function displayForecast(){
   $.ajax('/forecast').done(function(data){
-  var jour = data.dfn;
-  var meteo = data.weather;
+  var jour0 = data.weather[0].dfn;
+  var meteo0 = data.weather[0].weather;
+  var meteo1 = data.weather[1].weather;
 
-  if (jour == 0){
-    switch (weather) {
-      case rainy:
-      $("#todayForecast").html(<img class="logo" src="./include/rainy.png">) ;
-        break;
 
-      case cloudy:
-      $("#todayForecast").html(<img class="logo" src="./include/cloudy.jpg">);
-        break;
+  if (jour0 == 0) {
+      switch (meteo0) {
+        case "RAINY":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/rainy.png\">") ;
+          break;
 
-      case heatwave:
-      $("#todayForecast").html(<img class="logo" src="./include/heatwave.png">) ;
-        break;
+        case "CLOUDY":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/cloudy.jpg\">");
+          break;
 
-      case sunny:
-      $("#todayForecast").html(<img class="logo" src="./include/sunny.png">) ;
-        break;
+        case "HEATWAVE":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/heatwave.png\">") ;
+          break;
 
-      case thunderstorm:
-      $("#todayForecast").html(<img class="logo" src="./include/thunderstorm.png">) ;
-        break;
-      default:
-    }
-  }else if (jour == 1){
-    switch (weather) {
-      case rainy:
-      $("#tmrForecast").html(<img class="logo" src="./include/rainy.png">) ;
-        break;
+        case "SUNNY":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/sunny.png\">") ;
+          break;
 
-      case cloudy:
-      $("#tmrForecast").html(<img class="logo" src="./include/cloudy.jpg">);
-        break;
+        case "THUNDERSTORM":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/thunderstorm.png\">") ;
+          break;
+        default:
+      }
 
-      case heatwave:
-      $("#tmrForecast").html(<img class="logo" src="./include/heatwave.png">) ;
-        break;
+      switch (meteo1) {
+        case "RAINY":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/rainy.png\">") ;
+          break;
 
-      case sunny:
-      $("#tmrForecast").html(<img class="logo" src="./include/sunny.png">) ;
-        break;
+        case "CLOUDY":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/cloudy.jpg\">");
+          break;
 
-      case thunderstorm:
-      $("#tmrForecast").html(<img class="logo" src="./include/thunderstorm.png">) ;
-        break;
-      default:
-    }
+        case "HEATWAVE":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/heatwave.png\">") ;
+          break;
+
+        case "SUNNY":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/sunny.png\">") ;
+          break;
+
+        case "THUNDERSTORM":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/thunderstorm.png\">") ;
+          break;
+        default:
+  		  break;
+      }
+
+
+  } else if (jour0 == 1) {
+      switch (meteo0) {
+        case "RAINY":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/rainy.png\">") ;
+          break;
+
+        case "CLOUDY":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/cloudy.jpg\">");
+          break;
+
+        case "HEATWAVE":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/heatwave.png\">") ;
+          break;
+
+        case "SUNNY":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/sunny.png\">") ;
+          break;
+
+        case "THUNDERSTORM":
+        $("#tmrForecast").html("<img class=\"logo\" src=\"./include/thunderstorm.png\">") ;
+          break;
+        default:
+  		  break;
+      }
+
+      switch (meteo1) {
+        case "RAINY":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/rainy.png\">") ;
+          break;
+
+        case "CLOUDY":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/cloudy.jpg\">");
+          break;
+
+        case "HEATWAVE":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/heatwave.png\">") ;
+          break;
+
+        case "SUNNY":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/sunny.png\">") ;
+          break;
+
+        case "THUNDERSTORM":
+        $("#todayForecast").html("<img class=\"logo\" src=\"./include/thunderstorm.png\">") ;
+          break;
+        default:
+      }
   }
+})
 }
