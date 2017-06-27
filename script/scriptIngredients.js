62a256ab4417a45caa0f533d189407a097bed6ba
Unknown <fabien.mazette@gmail.com>
2017-06-27 16:09:30 +0200

    Correcting some bugs

--- script/scriptIngredients.js
+++ script/scriptIngredients.js
@@ -1,6 +1,21 @@
 var gameInfo = JSON.parse(localStorage.getItem("gameInfo"));
 console.log(gameInfo);
 var domain = "http://webserverlemonade.herokuapp.com";
+refreshPage()
+
 
 $(".name").text(gameInfo["name"]);
 console.log(gameInfo["name"])
+
+function refreshPage(){
+	//R5
+	$.get(domain+"/map/"+gameInfo.name).done(function(data){
+		var r5Resp = data;
+		gameInfo.info = r5Resp.playerInfo;
+		gameInfo.map = r5Resp.map;
+		gameInfo.availableIngredients = r5Resp.availableIngredients;
+		localStorage.setItem("gameInfo",JSON.stringify(gameInfo));
+		$("#cash").text(gameInfo["info"]["cash"]);
+		setTimeout(refreshPage,2000);
+	})
+};
