b2f04aded6905ceabce70ef001d67cd7e0ead101
Louis <louis.schneider@imerir.com>
2017-06-28 11:13:00 +0200

    Billboard inventory script adapted to server request, works locally, need TEST

--- script/scriptBb.js
+++ script/scriptBb.js
@@ -2,27 +2,35 @@ var gameInfo = JSON.parse(localStorage.getItem("gameInfo"));
 console.log(gameInfo);
 var domain = "http://webserverlemonade.herokuapp.com";
 
+var gameInfo = JSON.parse(localStorage.getItem("gameInfo"));
+console.log(gameInfo);
+var domain = "http://webserverlemonade.herokuapp.com";
+
 function dynamicBillboard(){
-	  $.ajax('/billboards').done(function(data){
+	  $.ajax('http://webserverlemonade.herokuapp.com/map').done(function(data){
 		  var i = 0;
+			var j = 0;
 		  $("#bbspawn").html("ta mere ");
-		  while (data[i] != ""){
-			  var latitude = data[i].location.latitude ;
-			  var longitude = data[i].location.longitude ;
-			  var taille = data[i].influence;
-			  if (taille < 50) var size = "Small" ;
-			  else var size = "Big";
+		  while (data.map.itemsbyplayer[i] != ""){
+				if(data.map.itemsbyplayer[i].kind == "ad"){
+					var latitude = data.map.itemsbyplayer[i].location.latitude ;
+				 	var longitude = data.map.itemsbyplayer[i].location.longitude ;
+				 	var taille = data.map.itemsbyplayer[i].influence;
+				 	if (taille < 30 && taille > 0) var size = "Small" ;
+					else if (taille >= 30 && taille < 50) var size = "Medium";
+					else if (taille >= 50 && taille < 80) var size = "Big" ;
+				 	else var size = "Fuckin\' Huge";
 
-			  //$("#bbspawn").html("<span><b>Number "+ (i+1) +":</b></span><ul><li>Position: <span>"+latitude+"</span> / <span>"+longitude+"</span></li> <li>Size: <span>"+size+"</span></li></ul>");
+				 	//$("#bbspawn").html("<span><b>Number "+ (i+1) +":</b></span><ul><li>Position: <span>"+latitude+"</span> / <span>"+longitude+"</span></li> <li>Size: <span>"+size+"</span></li></ul>");
 
-				//$("#mainTable tr:not(:first)").remove();
-				$("#bbTable tr:last").after("<tr> <td>"+(i+1)+"</td><td>"+latitude+"</td><td>"+longitude+"</td><td>"+size+"</td> </tr>");
-				 i++;
-
+				 	//$("#mainTable tr:not(:first)").remove();
+				 	$("#bbTable tr:last").after("<tr> <td>"+(j+1)+"</td><td>"+latitude+"</td><td>"+longitude+"</td><td>"+size+"</td> </tr>");
+					j++;
+					i++;
+					} else i++ ;
 				}
-
-	  })
-  }
+			})
+		}
 
 $(function(){
 	dynamicBillboard();
