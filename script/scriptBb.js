fea1302c84b86d4c05b69df8ad8a9ca65a217e29
Louis <louis.schneider@imerir.com>
2017-06-28 10:24:44 +0200

    Editing billboard inventory script, working locally. Next : adapt to serv request

--- script/scriptBb.js
+++ script/scriptBb.js
@@ -10,14 +10,16 @@ function dynamicBillboard(){
 			  var latitude = data[i].location.latitude ;
 			  var longitude = data[i].location.longitude ;
 			  var taille = data[i].influence;
-			  if (taille < 50) var size = "small" ;
-			  else var size = "big";
+			  if (taille < 50) var size = "Small" ;
+			  else var size = "Big";
 
-			  $("#bbspawn").html("ta mère ");
+			  //$("#bbspawn").html("<span><b>Number "+ (i+1) +":</b></span><ul><li>Position: <span>"+latitude+"</span> / <span>"+longitude+"</span></li> <li>Size: <span>"+size+"</span></li></ul>");
 
-			  $("#bbspawn").html("<span><b>Number "+ (i+1)+ ":</b></span><ul><li>Position: <span>"+latitude+"</span> / <span>"+longitude+"</span></li> <li>Size: <span>"+size+"</span></li></ul>");
-			  i++;
-		  }
+				//$("#mainTable tr:not(:first)").remove();
+				$("#bbTable tr:last").after("<tr> <td>"+(i+1)+"</td><td>"+latitude+"</td><td>"+longitude+"</td><td>"+size+"</td> </tr>");
+				 i++;
+
+				}
 
 	  })
   }
